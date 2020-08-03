55e6d115541f6157a27ee81d2c6913c1f43bd6cf
alexandre <amenezes031@gmail.com>
2020-08-03 17:08:40 -0300

    Add styles for landing page.

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,6 +7,8 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import './styles.css';
+
 function Landing() {
   return (
     <div id="page-landing">
@@ -32,8 +34,8 @@ function Landing() {
           </a>
 
           <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt="Ensinar"/>
-            Ensinar
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
           </a>
         </div>
 
